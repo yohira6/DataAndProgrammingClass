0e07dac8f986859ee6813b6283a9fb8e92557072
yohira6 <meme.ipad9@gmail.com>
2024-05-01 15:01:38 +0900

    ・段階を追加（負の数の場合メッセージを出す） ・タイトルを変更→小学一年生までの引き算をする処理 ・出力メソッドにif文を追加

--- Chap2/70311033_kadai1.py
+++ Chap2/70311033_kadai1.py
@@ -1,21 +1,26 @@
 #課題1:段階的詳細化
 #日にち:4月24日
 #学籍番号:70311033
-#どんな処理か:引き算をする処理　数を設定→数字を出力メソッドに返す→式にして表示
+#どんな処理か:小学生1年生までの引き算をする処理　1~9の数を設定→数字を出力メソッドに返す→式にして表示
+#数字を出力メソッドに返す→負の数の場合「まだ習ってないよ！」を表示
 
 
 #処理部分
+import random
+
 def number():
-    first = 10
-    second = 2
-    third = 4
+    first = random.randint(0 , 10)
+    second = random.randint(0 , 10)
+    third = random.randint(0 , 10)
     return first, second, third
 
 def print_subtraction(first, second, third):
-    print (first)
-    print (second)
-    print (third)
-    print (str(first) + "-" +  str(second) + "-" + str(third) + "=" + str(first - second - third) )
-
+    if(0 <= first - second - third):
+        print (first)
+        print (second)
+        print (third)
+        print (str(first) + "-" +  str(second) + "-" + str(third) + "=" + str(first - second - third) )
+    if(0 > first - second - third):
+        print ("まだ習ってないよ!" + "(式[" + str(first) + "-" +  str(second) + "-" + str(third) + "])")
 first, second, third = number()
 print_subtraction(first, second, third)
